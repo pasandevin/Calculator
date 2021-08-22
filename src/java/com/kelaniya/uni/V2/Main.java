b0939a14786f250e8ff4660acab051b37a8229fd
Pasan Jayawardene <pj799571@gmail.com>
2021-08-22 21:00:14 +0530

    v2 final code

--- src/java/com/kelaniya/uni/V2/Main.java
+++ src/java/com/kelaniya/uni/V2/Main.java
@@ -1,24 +1,48 @@
 package com.kelaniya.uni.V2;
 
-public class Main{
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 
-    public static void main(String[] args){
+public class Main {
 
-        // First validation to check if an cmd argument is provided
-        if(args.length == 0){
+    public static void main(String[] args) throws IOException {
+
+        // Validating the arguments before using
+        if (args.length == 0) {
             System.out.println("Please provide the operation as an argument");
             return;
         }
 
-        //Second validation 
         String operator = args[0];
 
-        if(!(operator.equals("add") || operator.equals("sub") || operator.equals("mul"))){
+        if (!(operator.equals("add") || operator.equals("sub") || operator.equals("mul"))) {
             System.out.println("Please provide add, sub or mul as the operator argument");
             return;
         }
 
-        System.out.println(args[0]);
+        //File reading
+        List<String> numbersStrs = Files.readAllLines(
+                Paths.get("C:\\Users\\Pasan Devin\\Desktop\\Calculator\\numbers.txt")
+        );
+
+        double number1 = Double.parseDouble(numbersStrs.get(0));
+        double number2 = Double.parseDouble(numbersStrs.get(1));
+
+        double result = 0;
+
+        //calculating part
+        if (operator.equals("add")) {
+            result = number1 + number2;
+        } else if (operator.equals("sub")) {
+            result = number1 - number2;
+        } else if (operator.equals("mul")) {
+            result = number1 * number2;
+        }
+
+        System.out.println("The result is " + result);
+
     }
 
 }
